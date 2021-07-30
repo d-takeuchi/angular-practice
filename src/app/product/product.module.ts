f6b98ac7a1dd8ee95880b039445559a05064a84e
d-takeuchi <kuronk.kuro3@gmail.com>
2021-07-30 17:29:23 +0900

    作業中

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,41 +1,35 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router'
-;
+import { Routes, RouterModule } from '@angular/router';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductListComponent } from './product-listings/product-listings.component';
+import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product.component';
-
+import { ProductService } from './shared/product.service';
 
 const routes: Routes = [
-  {
-    path:'product',
-    component: ProductComponent,
-    children:[
-      {
-        path:'',
-        component: ProductListComponent
-      },
-      {
-        path:':productId',
-        component:ProductDetailComponent
-      }
+  { 
+    path: 'products', component: ProductComponent,
+    children: [
+      { path: '', component: ProductListComponent },
+      { path: ':productId', component: ProductDetailComponent }
     ]
-  },
+  }
 ];
 
+
 @NgModule({
-  declarations:[
+  declarations: [
     ProductComponent,
-    ProductListComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    ProductListComponent
   ],
   imports: [
     RouterModule.forChild(routes),
     CommonModule
   ],
-  providers:[],
-  bootstrap:[]
+  providers: [
+    ProductService
+  ],
+  bootstrap: []
 })
-
 export class ProductModule { }
